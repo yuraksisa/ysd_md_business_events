ca2db24060f7510f74e1074ae917dafb74475e70
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2015-04-01 09:44:01 +0200

    refactoring

--- lib/ysd-md-business_event.rb
+++ lib/ysd-md-business_event.rb
@@ -1,5 +1,5 @@
 require 'data_mapper' unless defined?DataMapper
-
+require 'dm-constraints'
 module BusinessEvents
 
   #
@@ -14,12 +14,11 @@ module BusinessEvents
     
     storage_names[:default] = 'be_business_event'
    
-    property :id, Serial
+    property :id, Serial, :field => 'id', :key => true
     property :event, String, :field => 'event', :length => 32
     property :data, String, :field => 'data', :length => 1024 # It's the json representation of the data
-    property :date, DateTime, :field => 'date'                # When the event has happened
-    #has n, :business_event_processes, 'BusinessEvents::BusinessEventProcess', 
-    #  :child_key => [:business_event_id] , :parent_key => [:id], :constraint => :destroy
+    property :date, DateTime, :field => 'date'              # When the event has happened
+    has n, :business_event_processes, 'BusinessEventProcess', :child_key => [:business_event_id] , :parent_key => [:id], :constraint => :destroy
    
     # ================= Finders =================================
     
@@ -184,5 +183,64 @@ module BusinessEvents
     end    
          
   end #BusinessEvent
+  
+  #
+  # Represents the status of a BusinessEvent for an specific command
+  #
+  class BusinessEventProcess
+    include DataMapper::Resource
+    
+    storage_names[:default] = 'be_business_event_process'
+    
+    property :id, Serial, :field => 'id', :key => true
+    belongs_to :business_event, 'BusinessEvent', :child_key => [:business_event_id], :parent_key => [:id]
+    property :command_name, String, :field => 'command_name', :length => 32
+    
+    property :autoexecute, Boolean, :field => 'autoexecute', :default => false
+    property :status, String, :field => 'status', :length => 10, :default => :PENDING
+    property :last_update, DateTime, :field => 'last_update'
+    property :error, String, :field => 'error', :length => 255       
+ 
+    
+    #
+    # Before update 
+    #
+    before :save do
+      self.last_update = Time.now
+    end    
+    
+    #
+    # Process the command
+    #
+    def execute 
+       
+      puts "Executing the command : #{business_event.event} #{self.command_name} #{business_event.date} #{business_event.data}" 
+      
+      if command = BusinessEvent.get_command(business_event.event, self.command_name)
+      
+         command_class = command[:command_class]
+         command_instance = command_class.new(business_event)     
+       
+         begin
+           puts "Executing the command #{command_instance.class.name}"
+           command_instance.execute
+           attribute_set(:status, :DONE)
+           puts "Command executed successfully"
+         rescue
+           puts "Error executing the command"
+           attribute_set(:status, :ERROR)
+           attribute_set(:error, $!)
+         end
+       
+         # Update the data
+         if self.saved?
+           self.save
+         end
+         
+      end
+        
+    end
+    
+  end
   
 end #BusinessEvents
--- lib/ysd-md-business_events.rb
+++ lib/ysd-md-business_events.rb
@@ -1,3 +1,2 @@
 require 'ysd-md-business-events-command'
 require 'ysd-md-business_event'
-require 'ysd-md-business_event_process'
